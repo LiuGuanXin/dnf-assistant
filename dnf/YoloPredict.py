7a79f854d24cfb5af69a3776ff98bd5cb5ccf852
liuguangxin <l1401098954@gmail.com>
2024-06-18 22:35:13 +0800

    修改

---
+++ dnf/YoloPredict.py
@@ -0,0 +1,76 @@
+from ultralytics import YOLO
+
+
+class YoloPredict:
+    """初始化"""
+
+    def __init__(self, model_path):
+        self.data = None
+        self.box = None
+        self.results = None
+        if model_path is not None:
+            self.model = YOLO(model_path)
+        else:
+            self.model = YOLO("../model/best.pt")
+
+    """检测"""
+
+    def deal_img(self, img):
+        self.results = self.model(img)
+        self.data = self.results[0].boxes.data
+        # [505.0202, 169.7504, 651.0491, 249.8571, 0.8848, 1.]
+        # 0: self
+        # 1: monster
+        # 2: closeDoor
+        # 3: material
+        # 4: openDoor
+        # 左上角  右下角坐标  置信度 类别
+
+    """获取自身坐标"""
+
+    def get_self_cord(self):
+        x_min, y_min, x_max, y_max = 0, 0, 0, 0
+        for item in self.data:
+            if item[5] == 0 and item[4] > 0.7:
+                x_min, y_min, x_max, y_max = item[0], item[1], item[2], item[3]
+        return x_min, y_min, x_max, y_max
+
+    """获取怪物坐标"""
+
+    def get_monster_cord(self):
+        monster_cord_list = list()
+        for item in self.data:
+            if item[5] == 1 and item[4] > 0.7:
+                item = item[0:4]
+                monster_cord_list.append(item)
+        return monster_cord_list
+
+    """获取材料坐标"""
+
+    def get_material_cord(self):
+        material_cord_list = list()
+        for item in self.data:
+            if item[5] == 3 and item[4] > 0.7:
+                item = item[0:4]
+                material_cord_list.append(item)
+        return material_cord_list
+
+    """获取打开的门坐标"""
+
+    def get_open_door_cord(self):
+        open_door_cord_list = list()
+        for item in self.data:
+            if item[5] == 4 and item[4] > 0.7:
+                item = item[0:4]
+                open_door_cord_list.append(item)
+        return open_door_cord_list
+
+    """获取关闭的门坐标"""
+
+    def get_close_door_cord(self):
+        close_door_cord_list = list()
+        for item in self.data:
+            if item[5] == 2 and item[4] > 0.7:
+                item = item[0:4]
+                close_door_cord_list.append(item)
+        return close_door_cord_list
--- test.py
+++ test.py
@@ -1,36 +1,34 @@
 import cv2
 import pyautogui
 import numpy as np
-from YoloPredict import YoloPredict
-from Action import Action
-from Operate import Operate
+from dnf.YoloPredict import YoloPredict
+from dnf.Action import Action
+from dnf.Operate import Operate
 
 
-yolo = YoloPredict()
+yolo = YoloPredict("../model/best.pt")
 action = Action()
 operate = Operate()
 
 
-def screenshot():
+def screenshot(x, y, width, height):
     # 设置录制区域 (x, y, width, height)
-    region = (100, 100, 500, 400)
+    region = (x, y, width, height)
     # 捕获屏幕指定区域
-    frame = np.array(pyautogui.screenshot(region=region))
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    return frame
+    return cv2.cvtColor(np.array(pyautogui.screenshot(region=region)), cv2.COLOR_BGR2RGB)
 
 
 while True:
-    frame = screenshot()
+    frame = screenshot(1410, 875, 1153, 500)
     yolo.deal_img(frame)
-    self_cord = yolo.get_cord(0)
-    monster_cord = yolo.get_cord(1)
-    material_cord = yolo.get_cord(2)
-    open_door_cord = yolo.get_cord(2)
-    if monster_cord is not None:
-        action.attack(monster_cord)
-    if material_cord is not None:
-        action.pick_material()
+    self_cord = yolo.get_self_cord()
+    monster_cord = yolo.get_monster_cord()
+    material_cord = yolo.get_material_cord()
+    open_door_cord = yolo.get_open_door_cord()
+    if monster_cord is not None and self_cord is not None:
+        action.attack(self_cord, monster_cord)
+    if material_cord is not None and self_cord is not None:
+        action.pick_material(self_cord, material_cord)
     if (monster_cord is None
             and material_cord is None
             and open_door_cord is not None):
